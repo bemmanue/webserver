616f7b538947d2d87e8932018c163189711e378d
Burma Emmanuel <bemmanue@pr-i1.kzn.21-school.ru>
2023-01-31 18:08:58 +0300

    ok

--- include/Request.hpp
+++ include/Request.hpp
@@ -4,88 +4,13 @@
 #include <iostream>
 #include <map>
 
-enum StatusCode {
-	CONTINUE                        = 100,
-	SWITCHING_PROTOCOLS             = 101,
-	PROCESSING                      = 102,
-	EARLY_HINTS                     = 103,
-	OK                              = 200,
-	CREATED                         = 201,
-	ACCEPTED                        = 202,
-	NON_AUTHORITATIVE_INFORMATION   = 203,
-	NO_CONTENT                      = 204,
-	RESET_CONTENT                   = 205,
-	PARTIAL_CONTENT                 = 206,
-	MULTI_STATUS                    = 207,
-	ALREADY_REPORTED                = 208,
-	IM_USED                         = 226,
-	MULTIPLE_CHOICES                = 300,
-	MOVED_PERMANENTLY               = 301,
-	FOUND                           = 302,
-	SEE_OTHER                       = 303,
-	NOT_MODIFIED                    = 304,
-	USE_PROXY                       = 305,
-	TEMPORARY_REDIRECT              = 307,
-	PERMANENT_REDIRECT              = 308,
-	BAD_REQUEST                     = 400,
-	UNAUTHORIZED                    = 401,
-	PAYMENT_REQUIRED                = 402,
-	FORBIDDEN                       = 403,
-	NOT_FOUND                       = 404,
-	METHOD_NOT_ALLOWED              = 405,
-	NOT_ACCEPTABLE                  = 406,
-	PROXY_AUTHENTICATION_REQUIRED   = 407,
-	REQUEST_TIMEOUT                 = 408,
-	CONFLICT                        = 409,
-	GONE                            = 410,
-	LENGTH_REQUIRED                 = 411,
-	PRECONDITION_FAILED             = 412,
-	PAYLOAD_TOO_LARGE               = 413,
-	URI_TOO_LONG                    = 414,
-	UNSUPPORTED_MEDIA_TYPE          = 415,
-	RANGE_NOT_SATISFIABLE           = 416,
-	EXPECTATION_FAILED              = 417,
-	IM_A_TEAPOT                     = 418,
-	AUTHENTICATION_TIMEOUT          = 419,
-	MISDIRECTED_REQUEST             = 421,
-	UNPROCESSABLE_ENTITY            = 422,
-	LOCKED                          = 423,
-	FAILED_DEPENDENCY               = 424,
-	TOO_EARLY                       = 425,
-	UPGRADE_REQUIRED                = 426,
-	PRECONDITION_REQUIRED           = 428,
-	TOO_MANY_REQUESTS               = 429,
-	REQUEST_HEADER_FIELDS_TOO_LARGE = 431,
-	RETRY_WITH                      = 449,
-	UNAVAILABLE_FOR_LEGAL_REASONS   = 451,
-	CLIENT_CLOSED_REQUEST           = 499,
-	INTERNAL_SERVER_ERROR           = 500,
-	NOT_IMPLEMENTED                 = 501,
-	BAD_GATEWAY                     = 502,
-	SERVICE_UNAVAILABLE             = 503,
-	GATEWAY_TIMEOUT                 = 504,
-	HTTP_VERSION_NOT_SUPPORTED      = 505,
-	VARIANT_ALSO_NEGOTIATES         = 506,
-	INSUFFICIENT_STORAGE            = 507,
-	LOOP_DETECTED                   = 508,
-	BANDWIDTH_LIMIT_EXCEEDED        = 509,
-	NOT_EXTENDED                    = 510,
-	NETWORK_AUTHENTICATION_REQUIRED = 511,
-	UNKNOWN_ERROR                   = 520,
-	WEB_SERVER_IS_DOWN              = 521,
-	CONNECTION_TIMED_OUT            = 522,
-	ORIGIN_IS_UNREACHABLE           = 523,
-	A_TIMEOUT_OCCURRED              = 524,
-	SSL_HANDSHAKE_FAILED            = 525,
-	INVALID_SSL_CERTIFICATE         = 526
-};
+#include "StatusCode.hpp"
 
-#define SP		" "
-#define HTAB	"\t"
-#define CRLF	"\r\n"
-#define COLON	":"
-
-#define MAX_METHOD_LENGTH	6
+//#define SP		" "
+//#define HTAB	"\t"
+//#define CRLF	"\r\n"
+//#define COLON	":"
+//#define MAX_METHOD_LENGTH	6
 
 class Request {
 public:
@@ -95,6 +20,7 @@ public:
 
 	void	setMethod(const std::string& method);
 	void	setURI(const std::string& uri);
+	void	setQuery(const std::string& query);
 	void	setVersion(const std::string& version);
 	void	setHeaderField(const std::string& fieldName, const std::string& fieldValue);
 	void	setBody(const std::string& body);
@@ -103,20 +29,30 @@ public:
 private:
 	std::string							_method;
 	std::string							_uri;
+	std::string							_query;
 	std::string							_version;
 	std::map<std::string, std::string>	_headerFields;
 	std::string 						_body;
 	size_t								_status;
 
-	void		parseRequestLine(const std::string& request, size_t* i);
-	void		parseMethod(const std::string& request, size_t* i);
-	void		parseURI(const std::string& request, size_t* i);
-	void		parseVersion(const std::string& request, size_t* i);
-	void		parseHeaderFields(const std::string& request, size_t* i);
-	void		parseBody(const std::string& request, size_t* i);
-	void		skipSpace(const std::string& request, size_t* i);
-	std::string	getToken(const std::string& request, size_t* i);
+	void		parseRequestLine(const std::string& request, size_t* pos);
+	void		parseMethod(const std::string& request, size_t* pos);
+	void		parseURI(const std::string& request, size_t* pos);
+	void		parseVersion(const std::string& request, size_t* pos);
+	void		parseHeaderFields(const std::string& request, size_t* pos);
+	void		parseBody(const std::string& request, size_t* pos);
+	void		skipSpace(const std::string& request, size_t* pos);
+
+	std::string	getToken(const std::string& request, size_t pos);
+	std::string	getSegment(const std::string& request, size_t pos);
+	std::string	getAbsolutePath(const std::string& request, size_t pos);
+	std::string	getQuery(const std::string& request, size_t pos);
+
 	bool		isTchar(char a);
+	bool		isPchar(char a);
+	bool		isUnreserved(char a);
+	bool		isPctEncoded(const std::string& str);
+	bool		isSubDelim(char a);
 };
 
 #endif //REQUEST_HPP
--- src/Request.cpp
+++ src/Request.cpp
@@ -6,12 +6,15 @@ Request::Request(const std::string& request) {
 	size_t i = 0;
 
 	try {
-		parseRequestLine(request, &i);
-		parseHeaderFields(request, &i);
-		parseBody(request, &i);
 		setStatus(OK);
+		parseRequestLine(request, &i);
+//		parseHeaderFields(request, &i);
+//		parseBody(request, &i);
+		std::cout << _method << std::endl;
+		std::cout << _uri << std::endl;
+		std::cout << _version << std::endl;
 	} catch (const std::exception& ex) {
-		;
+		std::cerr << "Error code: " << _status << std::endl;
 	}
 }
 
@@ -25,32 +28,82 @@ void	Request::parseRequestLine(const std::string& request, size_t* i) {
 	parseVersion(request, i);
 }
 
-std::string	Request::getToken(const std::string& request, size_t* i) {
-	std::string token;
-
-	while (isTchar(request[*i])) {
-		token.push_back(request[(*i)++]);
-	}
-	return token;
-}
-
-
 void	Request::parseMethod(const std::string &request, size_t *i) {
 	std::string method;
 
-	method = getToken(request, i);
+	method = getToken(request, *i);
 	setMethod(method);
+	*i += method.size();
 }
 
 void	Request::parseURI(const std::string &request, size_t *i) {
 	std::string uri;
+	std::string query;
 
-	uri = getToken(request, i);
+	//origin form = absolute_path [? query]
+	//absolute_path = 1 * ("/" segment)
+
+	uri = getAbsolutePath(request, *i);
 	setURI(uri);
+	*i += uri.size();
+
+//	if (request[*i] == '?') {
+//		query = getQuery(request, *i);
+//		if (!query.empty()) {
+//			setQuery(query);
+//			*i += query.size();
+//		} else {
+//			throw std::exception();
+//		}
+//	}
+}
+
+std::string	Request::getAbsolutePath(const std::string &request, size_t i) {
+	std::string path;
+	std::string segment;
+
+	if (request[i] == '/') {
+		path.push_back(request[i++]);
+	} else {
+		setStatus(BAD_REQUEST);
+		throw std::exception();
+	}
+
+	while (segment = getSegment(request, i), !segment.empty()) {
+		path.append(segment);
+		i += segment.size();
+
+		if (request[i] == '/') {
+			path.push_back(request[i++]);
+		} else {
+			break;
+		}
+	}
+	return path;
+}
+
+std::string	Request::getQuery(const std::string &request, size_t i) {
+	std::string path;
+
+	return path;
 }
 
 void	Request::parseVersion(const std::string &request, size_t *i) {
+//	HTTP-name = %x48.54.54.50 ; HTTP
+//	HTTP-version = HTTP-name "/" DIGIT "." DIGIT
+	std::string version;
 
+	if (request.compare(*i, 5, "HTTP/")) {
+		setStatus(BAD_REQUEST);
+		throw std::exception();
+	}
+	version.append("HTTP/");
+	*i += 5;
+
+	if (isdigit(request[*i]) || ) {
+
+	}
+	setVersion(version);
 }
 
 void	Request::skipSpace(const std::string &request, size_t *i) {
@@ -58,8 +111,36 @@ void	Request::skipSpace(const std::string &request, size_t *i) {
 		++*i;
 	} else {
 		setStatus(BAD_REQUEST);
-		throw std::exception;
+		throw std::exception();
 	}
+}
+
+std::string	Request::getToken(const std::string& request, size_t i) {
+	std::string token;
+
+	while (isTchar(request[i])) {
+		token.push_back(request[i++]);
+	}
+	if (token.empty()) {
+		throw std::exception();
+	}
+	return token;
+}
+
+std::string Request::getSegment(const std::string &request, size_t i) {
+	std::string segment;
+
+	while (request[i]) {
+		if (isPchar(request[i])) {
+			segment.push_back(request[i++]);
+		} else if (isPctEncoded(request.substr(i, 3))) {
+			segment.append(request.substr(i, 3));
+			i += 3;
+		} else {
+			break;
+		}
+	}
+	return segment;
 }
 
 void	Request::parseHeaderFields(const std::string& request, size_t* i) {
@@ -89,12 +170,12 @@ void	Request::setMethod(const std::string &method) {
 	}
 }
 
-void	Request::setURI(const std::string &uri) {
-
+void	Request::setURI(const std::string& uri) {
+	_uri = uri;
 }
 
-void	Request::setVersion(const std::string &version) {
-
+void	Request::setVersion(const std::string& version) {
+	_version = version;
 }
 
 void	Request::setHeaderField(const std::string &fieldName, const std::string& fieldValue) {
@@ -106,16 +187,50 @@ void	Request::setBody(const std::string &body) {
 }
 
 void	Request::setStatus(size_t status) {
-
+	_status = status;
 }
 
 bool	Request::isTchar(char a) {
-	const char *tchars = "!#$%&'*+-.^_`|~";
+	const char *set = "!#$%&'*+-.^_`|~";
 
-	if (isalpha(a) ||
-		isdigit(a) ||
-		std::strchr(tchars, a)) {
+	if (isalpha(a) || isdigit(a) || std::strchr(set, a)) {
 		return true;
 	}
 	return false;
 }
+
+bool Request::isPchar(char a) {
+	if (isUnreserved(a) || isSubDelim(a) || std::strchr(":@", a)) {
+		return true;
+	}
+	return false;
+}
+
+bool Request::isUnreserved(char a) {
+	if (isalpha(a) || isdigit(a) || std::strchr("-._~", a)) {
+		return true;
+	}
+	return false;
+}
+
+bool Request::isPctEncoded(const std::string& str) {
+	if (str.size() == 3 &&
+		str[0] == '%' &&
+		ishexnumber(str[1]) &&
+		ishexnumber(str[2])) {
+		return true;
+	}
+	return false;
+}
+
+bool Request::isSubDelim(char a) {
+	if (std::strchr("!$&'()*+,;=", a)) {
+		return true;
+	}
+	return false;
+}
+
+void Request::setQuery(const std::string &query) {
+
+}
+
