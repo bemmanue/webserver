46daa0f63d97fdb73c465b9d9a688bb2ab1ae0ce
bemmanue <myaushenka@gmail.com>
2023-03-20 11:52:34 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "src/http/Response.hpp"
 #include "src/config/Config.hpp"
-#include "src/core/Connection.hpp"
 #include "src/core/Server.hpp"
 
 int main(int argc, char **argv) {
@@ -11,32 +10,14 @@ int main(int argc, char **argv) {
 		configs = parseConfigFile("conf/webserver.conf");
 	} catch (const std::exception& exception) {
 		std::cout << exception.what() << std::endl;
-		exit(0);
+		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < configs.size(); ++i) {
-		server.setConfig(configs[i]);
+	for (auto & config : configs) {
+		server.setConfig(config);
 	}
 
 	server.start();
-
-
-
-//	std::ifstream t("request.txt");
-//	if (!t.is_open()) {
-//		std::cout << "error openning request file" << std::endl;
-//		exit(0);
-//	}
-//
-//	t.seekg(0, std::ios::end);
-//	size_t size = t.tellg();
-//	std::string buffer(size, ' ');
-//	t.seekg(0);
-//	t.read(&buffer[0], size);
-//
-//	Connection client(config);
-//	client.setRequest(buffer);
-//	client.handleRequest();
 
     return 0;
 }
